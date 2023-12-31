4fb62210c1c77cbd84dbb4700235579545222edf
Khalid Bin Walid <kbwalid26@gmail.com>
2023-12-31 17:08:25 +0900

    update test

--- test/1.c
+++ test/1.c
@@ -1,3 +1,7 @@
-for (i = 0; i < 10; i = i + 1) {
-    printf("%d\n", i);
+void for_test() {
+    int i;
+    int printf(char *s, int i);
+    for (i = 0; i < 10; i = i + 1) {
+        printf("%d\n", i);
+    }
 }
